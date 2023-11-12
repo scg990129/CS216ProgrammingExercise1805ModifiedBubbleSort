1c700fd55e8c3aaf386c19aaae4e8ad3b186f536
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-11-12 14:59:00 -0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+// Modified Bubble Sort|
+// Write a complete C++ function template to implement the modified bubble sort algorithm
+// given in Exercise 9 of this chapter. Call this function modifiedBubbleSort.
+// Also, write a program to test your function.
---
+++ searchSortAlgorithms.h
@@ -0,0 +1,15 @@
+//
+// Created by Jacob Yeung on 11/12/23.
+//
+
+template <class elemType>
+void modifiedBubbleSort(elemType list[], int length)
+{
+    // Write code here
+}
+
+
+//#ifndef CS216PROGRAMMINGEXERCISE1805_SEARCHSORTALGORITHMS_H
+//#define CS216PROGRAMMINGEXERCISE1805_SEARCHSORTALGORITHMS_H
+//
+//#endif //CS216PROGRAMMINGEXERCISE1805_SEARCHSORTALGORITHMS_H

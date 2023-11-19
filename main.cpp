0acf6dcdd0ee5462f45265407da9f69c7294ffdd
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-11-18 22:14:55 -0800

    completed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include "searchSortAlgorithms.h"
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int list[] = {22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70};
+
+    int *ptr = list;
+    modifiedBubbleSort(ptr, 14);
+
+    for(auto i: list)
+        cout << i << " ";
+
+    int intList[] = {2, 56, 34, 25, 73,
+                     46, 89, 10, 5, 16};
+
+    double doubleList[] = {3.45, 2.34, 1.98, 55.43, 2.4353,4.5423};
+
+    modifiedBubbleSort(intList, 10);
+    modifiedBubbleSort(doubleList, 6);
+
+
+    for (int i : intList)
+        cout << i << " ";
+
+    cout << endl;
+
+    for (double i : doubleList)
+        cout << i << " ";
+
     return 0;
 }
 
@@ -11,3 +36,5 @@ int main() {
 // Write a complete C++ function template to implement the modified bubble sort algorithm
 // given in Exercise 9 of this chapter. Call this function modifiedBubbleSort.
 // Also, write a program to test your function.
+
+
--- searchSortAlgorithms.h
+++ searchSortAlgorithms.h
@@ -1,11 +1,33 @@
 //
 // Created by Jacob Yeung on 11/12/23.
 //
+using namespace std;
 
 template <class elemType>
 void modifiedBubbleSort(elemType list[], int length)
 {
-    // Write code here
+    bool flag;
+    elemType temp;
+    for(int i = 0; i < length; i++){
+        flag = false;
+        cout << "\nThe flag is false, length: " << i << endl;
+        for(int i = 0; i < length; i++){
+            cout << list[i] << " ";
+        }
+        cout << "\nThe go sort: " << i << endl;
+        for(int j=0; j < length-1-i; j++){
+            if(list[j]>list[j+1]){
+                temp = list[j];
+                list[j] = list[j+1];
+                list[j+1] = temp;
+                flag = true;
+            }
+        }
+        if(!flag){
+            cout << "The list is sorted." << endl;
+            break;
+        }
+    }
 }
 
 
